f86c0946b91783f31219fc9909a43edb120d3036
Allen Thich <allenthich@gmail.com>
2023-09-11 21:10:19 +0900

    Scaffold main view Chakra templates

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,49 @@ import {
   VStack,
   Code,
   Grid,
+  GridItem,
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./features/ColorModeSwitcher"
 import { Logo } from "./features/Logo"
+import Header from "./components/Header"
+import Inspector from "./components/Inspector"
+import SidePanel from "./components/SidePanel"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
+    <Grid
+      templateAreas={`"header"
+                      "body"`}
+      gridTemplateRows={'3rem calc(100vh - 3rem)'}
+      gridTemplateColumns={'1fr'}
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+      <GridItem pl='2' bg='orange.300' area={'header'}>
+        <Header />
+      </GridItem>
+      <Grid
+        templateAreas={`"lPanel main rPanel"`}
+        gridTemplateRows={'1fr'}
+        gridTemplateColumns={'250px 1fr 250px'}
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <GridItem pl='2' bg='pink.300' area={'lPanel'}> 
+          <SidePanel />
+        </GridItem>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+        <GridItem pl='2' bg='purple.300' area={'rPanel'}>
+        <Inspector />
+        </GridItem>
+      </Grid>
+    </Grid>
+    {/* <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
@@ -33,6 +68,6 @@ export const App = () => (
           </Link>
         </VStack>
       </Grid>
-    </Box>
+    </Box> */}
   </ChakraProvider>
 )
